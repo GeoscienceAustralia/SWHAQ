8fb8b087e3e3c609e7b307ff9f4abae101190c68
wcarthur <wcarthur@gmail.com>
2019-12-06 10:01:17 +1100

    First commit

---
+++ configuration/jobs/013-03564.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#PBS -Pw85
+#PBS -qexpress
+#PBS -N tc-013-03564
+#PBS -m ae
+#PBS -M craig.arthur@ga.gov.au
+#PBS -lwalltime=06:00:00
+#PBS -lmem=16GB,ncpus=1,jobfs=4000MB
+#PBS -joe
+module purge
+module load pbs
+module load dot
+module load openmpi/1.8.4
+module load python/2.7.11
+module load python/2.7.11-matplotlib
+module load pypar/26Feb15-2.7.6-1.8.4
+module load geos
+module load gdal/1.11.1-python
+
+module list
+DATE=`date +%Y%m%d%H%M`
+SIMULATION=013-03564
+OUTPUT=/g/data/w85/QFES_SWHA/wind/regional/$SIMULATION
+CONFIGFILE=/g/data/w85/QFES_SWHA/configuration/tcrm/$SIMULATION.ini
+PYTHONPATH=$PYTHONPATH:$HOME/tcrm:$HOME/tcrm/Utilities
+echo $PYTHONPATH
+echo $CONFIGFILE
+echo $OUTPUT
+echo $GEOS_ROOT
+
+# Ensure output directory exists. If not, create it:
+
+if [ ! -d "$OUTPUT" ]; then
+   mkdir $OUTPUT
+fi
+
+
+# Run the complete simulation:
+python $HOME/tcrm/tcevent.py -c $CONFIGFILE > $OUTPUT/$SIMULATION.stdout.$DATE 2>&1
+
+cd $OUTPUT
+cp $CONFIGFILE ./$SIMULATION.$DATE.ini
---
+++ configuration/jobs/swhaq_013-03564_pm.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#PBS -Pw85
+#PBS -qexpress
+#PBS -N tc-013-03564
+#PBS -m ae
+#PBS -M craig.arthur@ga.gov.au
+#PBS -lwalltime=01:00:00
+#PBS -lmem=32GB,ncpus=1,jobfs=4000MB
+#PBS -joe
+
+module purge
+module load pbs
+module load dot
+module load openmpi/1.8.4
+module load python/2.7.11
+module load python/2.7.11-matplotlib
+module load pypar/26Feb15-2.7.6-1.8.4
+module load geos
+module load gdal/1.11.1-python
+
+module list
+DATE=`date +%Y%m%d%H%M`
+SIMULATION=013-03564
+OUTPUT=/g/data/w85/QFES_SWHA/wind/local/$SIMULATION
+CONFIGFILE=/g/data/w85/QFES_SWHA/configuration/pm/QLD_$SIMULATION\_pm.ini
+PYTHONPATH=$PYTHONPATH:$HOME/tcrm:$HOME/tcrm/Utilities
+echo $PYTHONPATH
+echo $CONFIGFILE
+echo $OUTPUT
+echo $GEOS_ROOT
+# Run the complete simulation:
+python $HOME/tcrm/ProcessMultipliers/processMultipliers.py -c $CONFIGFILE > $OUTPUT/$SIMULATION.stdout.$DATE 2>&1
+
+cd $OUTPUT
+cp $CONFIGFILE ./$SIMULATION.$DATE.ini
