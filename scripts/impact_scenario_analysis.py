38d1f4cc1ca781f8467dce2b1da2644eed0b6871
wcarthur <craig.arthur@ga.gov.au>
2020-02-11 22:08:44 +0800

    Add post-processing scripts

---
+++ scripts/impact_scenario_analysis.py
@@ -0,0 +1,215 @@
+
+# coding: utf-8
+
+# # Impact scenario quality control
+# 
+# This notebook is intended to provide a review of the impact
+# scenarios generated in the Severe Wind Hazard Assessment project.
+# 
+# The impact results are generated using the
+# [_HazImp_](https://github.com/GeoscienceAustralia/hazimp) tool, with
+# TC wind footprints generated from
+# [TCRM](https://github.com/GeoscienceAustralia/tcrm). Scenarios were
+# selected in consultation with Department of Fire and Emergency
+# Services (Western Australia) and the Bureau of Meteorology (Western
+# Australia Regional Office).
+# 
+# ### Dependencies
+# 
+# 1. Numpy
+# 2. Pandas
+# 3. GeoPandas
+# 4. Matplotlib
+# 5. Seaborn
+# 6. HazImp
+# 
+# NOTE: I have only successfully installed GeoPandas on a Python 3.6
+# installation (on a Windows environment). 
+
+import matplotlib
+
+matplotlib.use("Agg")
+import os 
+from os.path import join as pjoin
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+sns.set_context('talk',font_scale=1.5)
+                #rc={"xtick.labelsize":"large", 
+                #    "ytick.labelsize":"large"})
+                
+sns.set_style('ticks')
+palette = sns.blend_palette(["#5E6A71", "#006983", "#72C7E7", "#A33F1F",
+                             "#CA7700", "#A5D867", "#6E7645"], 7)
+dmgpal = sns.blend_palette([(0.000, 0.627, 0.235), (0.412, 0.627, 0.235), 
+                            (0.663, 0.780, 0.282), (0.957, 0.812, 0.000), 
+                            (0.925, 0.643, 0.016), (0.835, 0.314, 0.118),
+                            (0.780, 0.086, 0.118)], 5)
+sns.set_palette(palette)
+
+
+# The output files for the impact scenarios are stored in the project
+# directory, and are stored as csv files.
+
+data_path = r"\\prod.lan\active\ops\community_safety\georisk\HaRIA_B_Wind\projects\qfes_swha\data\derived\impact"
+
+events = ['007-02914', '016-04518', '011-01326']
+
+res = 600
+context='talk'
+fmt = "png"
+
+# To review an event, change the event number in the following
+# line. This will load the different output file for inspection.
+for event_num in events:
+    print("Processing event {0}".format(event_num))
+
+    output_path = pjoin(data_path, event_num)
+    try:
+        os.makedirs(output_path)
+    except:
+        pass
+
+    event_file = pjoin(data_path, "QFES_{0}.csv".format(event_num))
+
+    YEAR_ORDER = ['Pre 1914', '1914 - 1946', '1947 - 1961',
+                  '1962 - 1981', '1982 - 1996', '1997 - present']
+    ROOF_TYPE_ORDER = ['Metal Sheeting', 'Tiles',
+                       'Fibro / asbestos cement sheeting']
+    WALL_TYPE_ORDER = ['Double Brick', 'Fibro / asbestos cement sheeting',
+                       'Timber', 'Brick Veneer']
+
+
+    df = pd.read_csv(event_file)
+
+
+    # Start with plotting the structural loss ratio against 10m wind
+    # speed. This should intuitively follow the vulnerability functions
+    # used in _HazImp_. Points are coloured by the construction age.
+    fig, ax = plt.subplots(figsize=(16,9))
+    sns.set_context(context,font_scale=1.)
+    g = sns.lmplot(x='0.2s gust at 10m height m/s',
+                   y='structural_loss_ratio', 
+                   hue='YEAR_BUILT',
+                   hue_order=YEAR_ORDER,
+                   data=df,
+                   fit_reg=False,
+                   aspect=2, legend_out=False)
+
+    g.ax.set_ylabel("Structural loss ratio")
+    g.ax.set_ylim((0, 1))
+    g.ax.set_xlim((20, 90))
+    g.ax.legend(title="Construction era", frameon=True)
+    plt.savefig(pjoin(output_path, "SLR_Windspeed_by_age.{0}".format(fmt)), dpi=res)
+    plt.close(fig)
+
+    # Examine the distribution of structural loss ratio within each
+    # contruction era, and colour the points by roof type (out of
+    # interests sake). For lower intensity events, we expect the newer
+    # construction (post-1982) to experience very low or no damage, since
+    # these events will generate wind speeds that are well below the
+    # design wind speeds for the region.
+
+    fig, ax = plt.subplots(figsize=(16,9))
+    sns.set_context(context,font_scale=1.5)
+
+    sns.stripplot(x='YEAR_BUILT', y='structural_loss_ratio', 
+                  hue='ROOF_TYPE', order=YEAR_ORDER, 
+                  data=df, jitter=True, ax=ax)
+    ax.set_xlabel("Construction era", fontsize='large')
+    #ax.set_ylim((0, 1))
+
+    ax.set_ylabel("Structural loss ratio", fontsize='large')
+    ax.legend(title="Roof material", frameon=True)
+    fig.savefig(pjoin(output_path, "SLR_by_age.{0}".format(fmt)), dpi=res,
+                bbox_inches="tight")
+    plt.close(fig)
+
+    # Now look at the structural loss in dollar terms. This is less
+    # important for an EM perspective, but serves as a cursory check that
+    # the outputs are sensible. Replacement costs are based on quantity
+    # surveyor data and represent 2017 values.
+    fig, ax = plt.subplots(figsize=(16,9))
+    sns.set_context(context,font_scale=1.)
+
+    g = sns.lmplot(x='0.2s gust at 10m height m/s',
+                  y='structural_loss', 
+                  hue='YEAR_BUILT', 
+                  hue_order=YEAR_ORDER,
+                  data=df,
+                  fit_reg=False,
+                  aspect=2, legend_out=False)
+
+    ylabels = g.ax.get_yticks()
+    g.ax.set_yticklabels(["${:0,.0f}".format(y) for y in ylabels])
+    g.ax.set_ylabel("Structural loss ($)")
+    g.ax.set_xlim((20, 90))
+    g.ax.legend(title="Construction era", frameon=True)
+    plt.savefig(pjoin(output_path, "SLC_Windspeed_by_age.{0}".format(fmt)), dpi=res,
+                bbox_inches="tight")
+    plt.close(fig)
+
+    bins=[0.0, 0.02, 0.1, 0.2, 0.5, 1.0]
+    labels=['Negligible', 'Slight', 'Moderate', 'Extensive', 'Complete']
+    df['Damage state'] = pd.cut(df['structural_loss_ratio'], bins,
+                                right=True, labels=labels)
+
+    # Save a table of number of buildings in each damage state per LGA:
+    df.pivot_table(index='LGA_NAME', columns='Damage state', aggfunc='size', fill_value=0).to_excel(pjoin(output_path, "{0}_damage_state_lga.xlsx".format(event_num)))
+
+    # Save a table of number of buildings in each damage state, broken
+    # down by construction era, roof type and wall type:
+    df.pivot_table(index=['Damage state', 'YEAR_BUILT'], 
+                   columns=['WALL_TYPE', 'ROOF_TYPE'], 
+                   aggfunc='size', fill_value=0).to_excel(pjoin(output_path, "{0}_damage_state_type.xlsx".format(event_num)))
+
+    # Save a table of number of buildings in each damage state, broken
+    # down by construction era
+    df.pivot_table(index='YEAR_BUILT', 
+                   columns='Damage state',
+                   aggfunc='size', 
+                   fill_value=0).to_excel(pjoin(output_path, "{0}_dmgstate.xlsx".format(event_num)))
+    fig, ax = plt.subplots(figsize=(16,9))
+    sns.set_context(context,font_scale=1.5)
+
+    sns.countplot(x='Damage state', data=df, order=labels, palette=dmgpal)
+    ax.set_ylabel("Number")
+    plt.savefig(pjoin(output_path, "damage_state.{0}".format(fmt)), 
+                dpi=res, bbox_inches="tight")
+    plt.close(fig)
+
+
+    fig, ax = plt.subplots(figsize=(16,9))
+    sns.set_context(context,font_scale=1.5)
+    ax = sns.countplot(x='Damage state', data=df, order=labels, hue='YEAR_BUILT', hue_order=YEAR_ORDER)
+    ax.legend(title="Construction era", frameon=True, loc=1)
+    ax.set_ylabel("Number")
+    plt.savefig(pjoin(output_path, "damage_state_by_age.{0}".format(fmt)),
+                dpi=res, bbox_inches="tight")
+    plt.close(fig)
+
+    fig, ax = plt.subplots(figsize=(16,9))
+    sns.set_context(context,font_scale=1.5)
+    ax = sns.pointplot(x='Damage state', y='structural_loss_ratio',
+                       data=df, order=labels,
+                       hue='YEAR_BUILT', hue_order=YEAR_ORDER)
+    ax.set_ylabel("Structural loss ratio")
+    ax.set_ylim((0, 1))
+    ax.legend(title="Construction era", frameon=True)
+    plt.savefig(pjoin(output_path, "SLR_by_damage_state.{0}".format(fmt)), 
+                dpi=res, bbox_inches="tight")
+    plt.close(fig)
+
+    # Building age profile for the community. 
+    fig, ax = plt.subplots(figsize=(16,9))
+    sns.set_context(context,font_scale=1.5)
+    sns.countplot(x='YEAR_BUILT', order=YEAR_ORDER, data=df, ax=ax)
+    ax.set_xlabel("Construction era")
+    ax.set_ylabel("Number")
+    plt.savefig(pjoin(output_path, "building_age_profile.{0}".format(fmt)),
+                dpi=res, bbox_inches="tight")
+    plt.close(fig)
---
+++ scripts/local_wind_extract_info_from_excel.py
@@ -0,0 +1,284 @@
+######################################################################
+# Name: local_wind_extract_info_from_excel.py.py
+# Author:  R. Coghlan
+# Date: 2018-01-23
+# Desc: Processes a folder of NEXIS exposure report outputs by extracting
+# required data from spreadsheets for each event-category and summarising 
+# them in a new table ready to include in map products
+#
+# 2019-03-19: Updated by C. Arthur to read in new format exposure reports
+#             generated via AEIP. 
+# 2019-11-12: Updated to Python 3 (C. Arthur)
+#
+# NOTES:
+# This assumes the filenames are like AEIP_Data_<Locality> Cat<#> <eventId> Cat[1-5].xlsx
+# where <Locality> is the name of the location, <#> represents the nominal category of the TC event 
+# (i.e. the intended maximum category), <eventId> is the scenario number (from TCHA) and Cat[1-5] is used
+# to identify which wind zone is being processed. For example:
+#    AIEP_Data_Cairns Cat5 013-03564 Cat3.xlsx represents the area exposed to category 3 wind speeds, for 
+#    a TC in Cairns with a maximum intenisty of category 5, and event Id number 013-03564.
+#
+######################################################################
+
+import xlrd 
+import numpy as np
+import pandas as pd
+import os
+import re
+from builtins import str
+import pdb
+
+# function to transpose table
+def transpose(inputList):
+    tmptest = []
+    for i in range(len(max(inputList, key=len))):
+        tmptest.append([(c[i] if i<len(c) else '') for c in inputList])
+    return tmptest
+    
+########################################################################
+# input directory where exposure .xls files for each category exist
+dir = r'\\prod.lan\active\ops\community_safety\georisk\HaRIA_B_Wind\projects\qfes_swha\data\derived\exposure\local_wind'
+########################################################################
+
+dirlist =  [x[0] for x in os.walk(dir)]
+dirlist.pop(0)
+for dir in dirlist:
+    print(('Processing {}'.format(dir)))
+
+    fileList = []
+    uniqueNameList = []
+    nameCheck = re.compile('AEIP_Data_.*Cat[0-9].*\.xlsx')
+    print((os.listdir(dir)))
+    # create a list of all excel files that match the regex above
+    for file in [f for f in os.listdir(dir) if nameCheck.search(f)]:
+        fileList.append(os.path.join(dir, file))
+        
+    print(fileList)
+    # for each file, remove the category off the filename and store if list if not there already    
+    for file in fileList:
+        eventName = os.path.basename(file).split(".")[0].rsplit('_', 1)[0]
+        eventName = os.path.basename(file).split(".")[0].rsplit('_', 1)[1].rsplit(' ', 1)[0]
+        print(eventName)
+        if eventName not in uniqueNameList:
+            uniqueNameList.append(eventName)
+    
+    # loop through each item in uniqueNameList
+    for uniqueEvent in uniqueNameList:
+        print(('Processing {}'.format(uniqueEvent)))
+        # create an empty dataframe results from each category will be appended to
+        masterdf = pd.DataFrame(columns=['Description','Cat1','Cat2','Cat3','Cat4','Cat5','Order'])
+        # loop through each file    
+        for file in fileList:
+            # if file matchs uniqueEvent, continue processing
+            if uniqueEvent in file:
+                print(('\tExtracting info from {}'.format(os.path.basename(file))))
+                # extract the cat number and store in a variable
+                cat = os.path.basename(file).split(".")[0].split(" ")[-1]
+    
+                # open the workbook
+                workbook = xlrd.open_workbook(file, encoding_override='utf-8') 
+                all_worksheets = workbook.sheet_names() 
+                for worksheet_name in all_worksheets:
+                    print(worksheet_name)
+                    # if the worksheet is Building Exposure extract the info and the transpose the table
+                    if worksheet_name == 'Building Exposure':
+                        BldExposure = []
+                        worksheet = workbook.sheet_by_name(worksheet_name) 
+                        worksheetAsAList = []
+                        for rownum in range(worksheet.nrows): 
+                            worksheetAsAList.append([str(entry).encode("utf-8") for entry in worksheet.row_values(rownum)]) 
+                   
+                        #transposedWorksheet = transpose(worksheetAsAList)
+                        
+                        # extract the following fields and append to list
+                        fieldsToExtract = ['Population count',
+                                           'Building count',
+                                           'Commercial Building Count',
+                                           'Industrial Building Count']
+                        #for row in transposedWorksheet:
+                        for row in worksheetAsAList:
+                            if row[1].decode('utf-8') in fieldsToExtract:
+                                #print(row[1].decode('utf-8'), row[2].decode('utf-8'))
+                                BldExposure.append([row[1].decode('utf-8'), float(row[2].decode('utf-8'))])
+                        # create a dataframe of the extracted info        
+                        BldExposuredf = pd.DataFrame(BldExposure, columns=['CAT', 'COUNT'])
+                        #print(BldExposuredf)
+
+                    # if worksheet is Infrastructure Exposure extract the relevent fields to a list
+                    if worksheet_name == 'Institution Exposure':
+                        InstitutionExposure = []
+                        worksheet = workbook.sheet_by_name(worksheet_name)
+                        worksheetAsAList = []
+                        for rownum in range(worksheet.nrows):
+                            worksheetAsAList.append([str(entry).encode("utf-8") for entry in worksheet.row_values(rownum)])
+                        EdfieldsToExtract = ['School - Pre/Primary',
+                                           'School - Secondary',
+                                           'School - Tertiary',
+                                           'School - Other']
+                        HealthfieldsToExtract = ['Hospital - Public',
+                                           'Hospital - Private',
+                                           'Nursing Home',
+                                           'Retirement Home']        
+                        EmergencyfieldsToExtract = ['Police Station',
+                                           'Fire Station',
+                                           'Ambulance Station',
+                                           'SES Facility',
+                                           'Rural/Country Fire Facility',]
+                        for row in worksheetAsAList:
+                            if row[1].decode('utf-8') in EdfieldsToExtract:
+                                InstitutionExposure.append([row[1].decode('utf-8'), float(row[2].decode('utf-8')), 'Education'])
+                            if row[1].decode('utf-8') in HealthfieldsToExtract:
+                                InstitutionExposure.append([row[1].decode('utf-8'), float(row[2].decode('utf-8')), 'Health'])
+                            if row[1].decode('utf-8') in EmergencyfieldsToExtract:
+                                InstitutionExposure.append([row[1].decode('utf-8'), float(row[2].decode('utf-8')), 'Emergency'])
+
+                    if worksheet_name == 'Infrastructure Exposure':
+                        InfrastructureExposure = []
+                        worksheet = workbook.sheet_by_name(worksheet_name) 
+                        worksheetAsAList = []
+                        for rownum in range(worksheet.nrows): 
+                            worksheetAsAList.append([str(entry).encode("utf-8") for entry in worksheet.row_values(rownum)])
+                        EdfieldsToExtract = ['School - Pre/Primary',
+                                           'School - Secondary',
+                                           'School - Tertiary',
+                                           'School - Other (Combined, Special)']
+                        HealthfieldsToExtract = ['Hospital - Public',
+                                           'Hospital - Private',
+                                           'Nursing Home',
+                                           'Retirement Home']        
+                        EmergencyfieldsToExtract = ['Police Station',
+                                           'Fire Station',
+                                           'Ambulance Station',
+                                           'SES Facility',
+                                           'Emergency Management Facility',]
+                        RoadfieldsToExtract = ['Road - Major (km)',
+                                            'Road - Arterial and Sub-arterial (km)']
+                        RailfieldsToExtract = ['Railway - Tracks (km)']
+                        EleclfieldsToExtract = ['Transmission - Electricity Lines (km)']
+                        
+                        # append extracted info to list and add catagory field
+                        for row in worksheetAsAList:
+                            if row[1].decode('utf-8') in EdfieldsToExtract:
+                                InfrastructureExposure.append([row[1].decode('utf-8'), float(row[2].decode('utf-8')), 'Education'])
+                            if row[1].decode('utf-8') in HealthfieldsToExtract:
+                                InfrastructureExposure.append([row[1].decode('utf-8'), float(row[2].decode('utf-8')), 'Health'])
+                            if row[1].decode('utf-8') in EmergencyfieldsToExtract:
+                                InfrastructureExposure.append([row[1].decode('utf-8'), float(row[2].decode('utf-8')), 'Emergency'])    
+                            if row[1].decode('utf-8') in RoadfieldsToExtract:
+                                InfrastructureExposure.append([row[1].decode('utf-8'), float(row[2].decode('utf-8')), 'Roads'])
+                            if row[1].decode('utf-8') in RailfieldsToExtract:
+                                InfrastructureExposure.append([row[1].decode('utf-8'), float(row[2].decode('utf-8')), 'Rail'])
+                            if row[1].decode('utf-8') in EleclfieldsToExtract:
+                                InfrastructureExposure.append([row[1].decode('utf-8'), float(row[2].decode('utf-8')), 'Elec'])
+                    
+                
+                institutiondf = pd.DataFrame(InstitutionExposure, columns=['FEATURE_TYPE', 'COUNT', 'CAT'])
+                institutiondf = institutiondf.replace('-', np.nan)
+                institutiondf['COUNT'] = institutiondf.COUNT.astype(float)
+                # create dataframe from list
+                infrastructuredf = pd.DataFrame(InfrastructureExposure, columns=['FEATURE_TYPE', 'COUNT', 'CAT'])
+                # replace any dashs with NaN            
+                infrastructuredf = infrastructuredf.replace('-', np.nan)
+                # make sure count field is float no object
+                infrastructuredf['COUNT'] = infrastructuredf.COUNT.astype(float)
+                 
+                # replace any dashs with NaN            
+                BldExposuredf = BldExposuredf.replace('-', np.nan)
+                # make sure count field is float no object
+                BldExposuredf['COUNT'] = BldExposuredf.COUNT.astype(float)  
+                
+                # merge dataframes to form new one
+                df_new = pd.concat([BldExposuredf, institutiondf, infrastructuredf], sort=False)
+                
+                # group by the CAT field and sum the counts
+                finalDf = df_new.groupby('CAT')[['COUNT']].sum()
+
+                # convert the index field to a new field called Description
+                finalDf['Description'] = finalDf.index
+                # reset the infex
+                finalDf.reset_index(drop=True, inplace=True)
+                # setup columns for new dataframe
+                cols = ['Description', 'COUNT']
+                # create new dataqframe using columns above
+                finalDf = finalDf[cols] 
+                # add in empty field to store row order
+                finalDf["Order"] = ""
+                
+                # calculate row order based on description text
+                for i, row in finalDf.iterrows():
+                    if row['Description'] == 'Population count':
+                        finalDf.at[i,'Order'] = 1
+                    if row['Description'] == 'Building count':
+                        finalDf.at[i,'Order'] = 2
+                    if row['Description'] == 'Commercial Building Count':
+                        finalDf.at[i,'Order'] = 3        
+                    if row['Description'] == 'Industrial Building Count':
+                        finalDf.at[i,'Order'] = 4        
+                    if row['Description'] == 'Emergency':
+                        finalDf.at[i,'Order'] = 5        
+                    if row['Description'] == 'Health':
+                        finalDf.at[i,'Order'] = 6        
+                    if row['Description'] == 'Education':
+                        finalDf.at[i,'Order'] = 7        
+                    if row['Description'] == 'Roads':
+                        finalDf.at[i,'Order'] = 8        
+                    if row['Description'] == 'Rail':
+                        finalDf.at[i,'Order'] = 9        
+                    if row['Description'] == 'Elec':
+                        finalDf.at[i,'Order'] = 10        
+                
+                # rename the count field to the cat number extract from the spreadsheet filename            
+                finalDf = finalDf.rename(columns={'COUNT': '{0}'.format(cat)})
+                # append results to the master dataframe created at the beginning
+                masterdf = masterdf.append(finalDf)
+    
+                
+        print('\tMerging extracted data')
+
+        # ensure all count and order fields are flaot not object
+        for col in ['Cat1', 'Cat2', 'Cat3', 'Cat4', 'Cat5', 'Order']:
+            masterdf[col] = masterdf[col].astype('float')
+        
+        # convert any NaN values to 0    
+        masterdf.fillna(0, inplace=True)
+        # groupby the Description field to shrink the table down to single rows
+        masterdf = masterdf.groupby(['Description']).max()
+      
+        # convert the index field to a new field called Description
+        masterdf['Description'] = masterdf.index
+        # reset the infex
+        masterdf.reset_index(drop=True, inplace=True)
+        # change the order of the columns back to what we want
+        cols = ['Description','Cat1','Cat2','Cat3','Cat4','Cat5','Order']
+        masterdf = masterdf[cols] 
+    
+        
+    
+        # replace the description text with what is required for the output table
+        masterdf['Description'].replace('Population count', 'Number of people', inplace=True)
+        masterdf['Description'].replace('Building count', 'Number of residential buildings', inplace=True)
+        masterdf['Description'].replace('Commercial Building Count', 'Number of commercial buildings', inplace=True)
+        masterdf['Description'].replace('Industrial Building Count', 'Number of industrial buildings', inplace=True)
+        masterdf['Description'].replace('Emergency', 'Number of Emergency facilities', inplace=True)
+        masterdf['Description'].replace('Health', 'Number of health facilities', inplace=True)
+        masterdf['Description'].replace('Education', 'Number of school facilities', inplace=True)
+        masterdf['Description'].replace('Roads', 'Length of major roads (km)', inplace=True)
+        masterdf['Description'].replace('Rail', 'Length of railway lines (km)', inplace=True)
+        masterdf['Description'].replace('Elec', 'Length of electricity transmission Lines (km)', inplace=True)
+        
+        # sort the table based on the order field
+        masterdf.sort_values(by=['Order'], inplace=True)
+        # drop the order field from the table
+        masterdf = masterdf.drop(['Order'], axis=1)
+        
+        
+        #setup output filename and write output file
+        outputFile = '{}_local_wind_summary.xlsx'.format(uniqueEvent)
+        writer = pd.ExcelWriter(os.path.join(dir, outputFile))
+        masterdf.to_excel(writer,'Exposure_Summary', index=False)
+        writer.save()
+        print(('\tOutput written: {}'.format(os.path.join(dir, outputFile))))
+        print('\n')
+    
+
+print('Complete')
