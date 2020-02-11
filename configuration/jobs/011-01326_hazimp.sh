fd24f7d01d5045397290805d257c95e639a72f54
wcarthur <wcarthur@gmail.com>
2020-02-11 21:46:46 +1100

    Fix Hazimp config files
    Start adding job scripts to run hazimp on gadi

--- configuration/jobs/011-01326_hazimp.sh
+++ configuration/jobs/011-01326_hazimp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -Pw85
 #PBS -qexpress
-#PBS -N tc-016-04518
+#PBS -N 011-01326-hazimp
 #PBS -m ae
 #PBS -M craig.arthur@ga.gov.au
-#PBS -lwalltime=01:30:00
-#PBS -lmem=32GB,ncpus=1,jobfs=4000MB
+#PBS -lwalltime=02:00:00
+#PBS -lmem=16GB,ncpus=16,jobfs=4000MB
 #PBS -joe
 #PBS -lstorage=gdata/w85
 
@@ -18,10 +18,11 @@ module load netcdf/4.6.3
 module load hdf5/1.10.5
 module load geos/3.8.0
 module load proj/6.2.1
-module load gdal/3.0.2
+module load gdal/3.0.2 
+module load openmpi/4.0.1
 
 # Need to ensure we get the correct paths to access the local version of gdal bindings. 
-# The module versions are compiled against Python3.6
+# The module versions are compiled against Python3.6, so we can't use them
 export PYTHONPATH=/g/data/w85/.local/lib/python3.7/site-packages:$PYTHONPATH
 
 # Add the local Python-based scripts to the path:
@@ -29,35 +30,31 @@ export PATH=/g/data/w85/.local/bin:$PATH
 
 # Needs to be resolved, but this suppresses an error related to HDF5 libs
 export HDF5_DISABLE_VERSION_CHECK=2
+SOFTWARE=/g/data/w85/software
+
+# Add HazImp code to the path:
+export PYTHONPATH=$SOFTWARE/hazimp:$PYTHONPATH
+
+cd $SOFTWARE/hazimp/
 
 module list
 DATE=`date +%Y%m%d%H%M`
-SIMULATION=016-04518
-OUTPUT=/g/data/w85/QFES_SWHA/wind/local/$SIMULATION
-CONFIGFILE=/g/data/w85/QFES_SWHA/configuration/pm/QLD_$SIMULATION\_pm.ini
+SIMULATION=011-01326
 
-# Add path to where TCRM is installed.
-SOFTWARE=/g/data/w85/software
-BRANCH=master
-#SOFTWARE=$HOME
-
-# Add to the Python path. We need to ensure we set the paths in the correct order
-# to access the locally installed version of the GDAL bindings
-export PYTHONPATH=$PYTHONPATH:$SOFTWARE/tcrm/$BRANCH:$SOFTWARE/tcrm/$BRANCH/Utilities
+CONFIGFILE=/g/data/w85/QFES_SWHA/configuration/hazimp/$SIMULATION.yaml
+OUTPUT=/g/data/w85/QFES_SWHA/impact_
 
 echo $PYTHONPATH
 echo $CONFIGFILE
-echo $OUTPUT
-echo $GEOS_ROOT
 
 # Ensure output directory exists. If not, create it:
-
 if [ ! -d "$OUTPUT" ]; then
    mkdir $OUTPUT
 fi
 
 # Run the complete simulation:
-python3 $SOFTWARE/tcrm/$BRANCH/ProcessMultipliers/processMultipliers.py -c $CONFIGFILE > $OUTPUT/$SIMULATION.stdout.$DATE 2>&1
+python3 $SOFTWARE/hazimp/hazimp/main.py -c $CONFIGFILE > $OUTPUT/$SIMULATION.stdout.$DATE 2>&1
 
 cd $OUTPUT
 cp $CONFIGFILE ./$SIMULATION.$DATE.ini
+
