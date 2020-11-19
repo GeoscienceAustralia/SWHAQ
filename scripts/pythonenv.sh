faa24d9a982bde1cb12426b206bd77d4ab631eb2
wcarthur <wcarthur@gmail.com>
2020-11-19 12:29:47 +1100

    Set up consistent environment on gadi

---
+++ scripts/pythonenv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+module load python3/3.7.4
+module load netcdf/4.6.3
+module load hdf5/1.10.5
+module load geos/3.8.0
+module load proj/6.2.1
+module load gdal/3.0.2
+module load openmpi
+
+# Need to ensure we get the correct paths to access the local version of gdal bindings. 
+# The module versions are compiled against Python3.6
+export PYTHONPATH=/g/data/w85/.local/lib/python3.7/site-packages:$PYTHONPATH
+
+# Add the local Python-based scripts to the path:
+export PATH=/g/data/w85/.local/bin:$PATH
+
+# Needs to be resolved, but this suppresses an error related to HDF5 libs
+export HDF5_DISABLE_VERSION_CHECK=2
+
+#module list
+
+SOFTWARE=/g/data/w85/software
+export PYTHONPATH=$PYTHONPATH:$SOFTWARE/tcrm/develop:$SOFTWARE/tcrm/develop/Utilities
