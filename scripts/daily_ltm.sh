b5d0c5556048bc758acbceaadcc8158122994d23
wcarthur <wcarthur@gmail.com>
2021-08-24 20:47:49 +1000

    Updated scripts for extracting PI data

--- scripts/daily_ltm.sh
+++ scripts/daily_ltm.sh
@@ -1,31 +1,66 @@
 #!/bin/bash
-FILELIST=/scratch/w85/cxa547/swhaq/data/*.1980t2099.nc
+#PBS -Pw85
+#PBS -qnormal
+#PBS -N dailyltmpi
+#PBS -m ae
+#PBS -M craig.arthur@ga.gov.au
+#PBS -lwalltime=06:00:00
+#PBS -lmem=32GB,ncpus=16,jobfs=4GB
+#PBS -W umask=0002
+#PBS -joe
+#PBS -lstorage=gdata/w85
+
+module purge
+module load pbs
+module load dot
+
+module load cdo
+
+INPUTPATH=/g/data/w85/QFES_SWHA/hazard/input/pi/Daily/
+OUTPUTPATH=/g/data/w85/QFES_SWHA/hazard/input/pi/dltm/
+
+process()
+{
+    # Run a bunch of cdo commands:
+    # Args:
+    # $1 = start year
+    # $2 = end year
+    # $3 = input file name
+    # $4 = substring for the selected model
+    
+    echo "Processing $3"
+    cdo -ydaymean -selyear,$1/$2 $3 $OUTPUTPATH/$4.$1-$2.ltm.nc
+}
+
+cd $INPUTPATH
+FILELIST=*.1980t2099.nc
 
 for f in $FILELIST; do
   echo $f
   mdl=`echo $f | awk -F. '{print $1}'`
   echo $mdl
-  startyear=1980
-  endyear=2010
-  cmd="cdo -ydaymean -selyear,$startyear/$endyear $mdl.1980t2099.nc $mdl.$startyear-$endyear.ltm.nc"
-  echo $cmd
-  $cmd
 
-  startyear=2020
-  endyear=2039
-  cdo -ydaymean -selyear,$startyear/$endyear $mdl.1980t2099.nc $mdl.$startyear-$endyear.ltm.nc
-
-  startyear=2040
-  endyear=2059
-  cdo -ydaymean -selyear,$startyear/$endyear $mdl.1980t2099.nc $mdl.$startyear-$endyear.ltm.nc
-
-  startyear=2060
-  endyear=2079
-  cdo -ydaymean -selyear,$startyear/$endyear $mdl.1980t2099.nc $mdl.$startyear-$endyear.ltm.nc
-
-  startyear=2080
-  endyear=2099
-  cdo -ydaymean -selyear,$startyear/$endyear $mdl.1980t2099.nc $mdl.$startyear-$endyear.ltm.nc
+  process 1980 2010 $f $mdl
+  process 2020 2039 $f $mdl
+  process 2040 2059 $f $mdl
+  process 2060 2079 $f $mdl
+  process 2080 2099 $f $mdl
 
 done
 
+
+# Now do the RCP45 data - only covers 2004-2099
+
+FILELIST=*.2004t2099.nc
+
+for f in $FILELIST; do
+    echo $f
+    mdl=`echo $f | awk -F. '{print $1}'`
+    echo $mdl
+
+    process 2020 2039 $f $mdl
+    process 2040 2059 $f $mdl
+    process 2060 2079 $f $mdl
+    process 2080 2099 $f $mdl
+
+done
---
+++ scripts/monthlymean.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+#PBS -Pw85
+#PBS -qnormal
+#PBS -N monmeanpi
+#PBS -m ae
+#PBS -M craig.arthur@ga.gov.au
+#PBS -lwalltime=06:00:00
+#PBS -lmem=32GB,ncpus=16,jobfs=4GB
+#PBS -W umask=0002
+#PBS -joe
+#PBS -lstorage=gdata/w85
+
+# This script processes daily potential intensity files into monthly mean and maximum values for a range of time periods
+# We calculate monthly mean, minimum and maximum values for all variables in the PI files (vmax and pmin)
+# All data ends up in a single folder, so you may want to move it around after processing is complete.
+# Takes about 90 minutes on NCI's gadi machine - YMMV depending on processor speed and I/O.
+
+
+module purge
+module load pbs
+module load dot
+
+module load cdo
+
+INPUTPATH=/g/data/w85/QFES_SWHA/hazard/input/pi/Daily/
+OUTPUTPATH=/g/data/w85/QFES_SWHA/hazard/input/pi/monmean/
+
+process()
+{
+    # Run a bunch of cdo commands:
+    echo "Processing $3"
+    cdo -monmean -selyear,$1/$2 $3 $OUTPUTPATH/$4.$1.$2.monmean.nc
+    cdo -monmin -selyear,$1/$2 $3 $OUTPUTPATH/$4.$1.$2.monmin.nc
+    cdo -monmax -selyear,$1/$2 $3 $OUTPUTPATH/$4.$1.$2.monmax.nc
+}
+
+cd $INPUTPATH
+
+# This is the RCP85 data:
+FILELIST=*.1980t2099.nc
+
+for f in $FILELIST; do
+  echo $f
+  mdl=`echo $f | awk -F. '{print $1}'`
+  echo $mdl
+
+  process 1980 2010 $f $mdl
+  process 2020 2039 $f $mdl
+  process 2040 2059 $f $mdl
+  process 2060 2079 $f $mdl
+  process 2080 2099 $f $mdl
+
+done
+
+
+# Now do the RCP45 data - only covers 2004-2099 so we drop the 1980-2010 period
+cd $INPUTPATH
+FILELIST=*.2004t2099.nc
+
+for f in $FILELIST; do
+  echo $f
+  mdl=`echo $f | awk -F. '{print $1}'`
+  echo $mdl
+
+  process 2020 2039 $f $mdl
+  process 2040 2059 $f $mdl
+  process 2060 2079 $f $mdl
+  process 2080 2099 $f $mdl
+
+done
+
