9da0a02fa28261d8f68e25c8bd2d06bd0fbcaf62
Kieran Ricardo <kieranricardo@hotmail.com>
2021-11-22 10:42:07 +1100

    fixup indexing

--- scripts/extract_ts_indices.py
+++ scripts/extract_ts_indices.py
@@ -1,11 +1,35 @@
 import numpy as np
 import os
 import xarray as xr
-import pandas as pd
 from calendar import monthrange
-import time
-from datetime import datetime
 from mpi4py import MPI
+
+
+def dask_index(arr, indexes):
+    # assumes doing the second axis
+    sz1 = arr.data.shape[1] * arr.data.shape[2] * arr.data.shape[3]
+    sz2 = indexes.shape[1] * indexes.shape[2]
+    idxs = indexes.data.flatten() * sz2 + sz1 * (np.arange(indexes.size) // sz2) + np.arange(indexes.size) % sz2
+    # sort indices for speed
+    y = idxs.argsort()
+    i = np.empty_like(y)
+    i[y] = np.arange(y.size)
+    return arr.data.flatten()[idxs[y]].compute()[i].reshape(indexes.shape)
+
+
+def dask_interpolate(u, v, z, height):
+    top = height * 9.80665
+    idxs = (abs(top - z) + 1_000_000_000 * (z > top)).argmin(axis=1).compute()
+    z1 = dask_index(z, idxs)
+    u1 = dask_index(u, idxs)
+    v1 = dask_index(v, idxs)
+    z2 = dask_index(z, idxs - 1)
+    u2 = dask_index(u, idxs - 1)
+    v2 = dask_index(v, idxs - 1)
+    u_interp = (u2 * (top - z1) + u1 * (z2 - top)) / (z2 - z1)
+    v_interp = (v2 * (top - z1) + v1 * (z2 - top)) / (z2 - z1)
+    return u_interp, v_interp
+
 
 comm = MPI.COMM_WORLD
 
@@ -36,27 +60,26 @@ for year in rank_years:
         u = xr.open_dataset(ufile, chunks='auto').u.sel(longitude=long_slice, latitude=lat_slice)
         v = xr.open_dataset(vfile, chunks='auto').v.sel(longitude=long_slice, latitude=lat_slice)
         cape = xr.open_dataset(capefile, chunks='auto').cape.sel(longitude=long_slice, latitude=lat_slice).compute()
-        u10 = xr.open_dataset(u10file, chunks='auto').u10.cape.sel(longitude=long_slice, latitude=lat_slice).compute()
-        v10 = xr.open_dataset(u10file, chunks='auto').v10.cape.sel(longitude=long_slice, latitude=lat_slice).compute()
-        totalx = xr.open_dataset(totalxfile, chunks='auto').u10.cape.sel(longitude=long_slice, latitude=lat_slice).compute()
-        z = xr.open_dataset(zfile, chunks='auto').z.sel(longitude=long_slice, latitude=lat_slice).compute()
+        u10 = xr.open_dataset(u10file, chunks='auto').u10.sel(longitude=long_slice, latitude=lat_slice).compute()
+        v10 = xr.open_dataset(v10file, chunks='auto').v10.sel(longitude=long_slice, latitude=lat_slice).compute()
+        totalx = xr.open_dataset(totalxfile, chunks='auto').totalx.sel(longitude=long_slice, latitude=lat_slice).compute()
+        z = xr.open_dataset(zfile, chunks='auto').z.sel(longitude=long_slice, latitude=lat_slice)
 
-        speed500 = np.sqrt(u.sel(level=500).compute() ** 2 + v.sel(pressure=500).compute() ** 2)
+        speed500 = np.sqrt(u.sel(level=500).compute() ** 2 + v.sel(level=500).compute() ** 2)
         speed10 = np.sqrt(u10 ** 2 + v10 ** 2)
 
         mason = (speed500 - speed10) * cape ** 1.67
-        mason = mason.data.reshape((-1, 24)).max(axis=1)
+        mason = mason.data.reshape((-1, 24) + mason.data.shape[1:]).max(axis=1)
 
-        idxs = abs(6_000 * 9.80665 - z).argmin(axis=0).compute()
-        utop = u.data.flatten()[idxs.flatten() * idxs.size + np.arange(idxs.size)].reshape(idxs.shape).compute()
-        vtop = v.data.flatten()[idxs.flatten() * idxs.size + np.arange(idxs.size)].reshape(idxs.shape).compute()
+        totalx = totalx.data.reshape((-1, 24) + totalx.data.shape[1:]).max(axis=1)
 
+        utop, vtop = dask_interpolate(u, v, z, height=6_000)
         uu = utop - u10
         vv = vtop - v10
         shear = np.sqrt(uu ** 2 + vv ** 2)
         allen = shear * cape ** 1.67
-        allen = allen.data.reshape((-1, 24)).max(axis=1)
+        allen = allen.data.reshape((-1, 24) + allen.data.shape[1:]).max(axis=1)
 
-        totalx = totalx.data.reshape((-1, 24)).max(axis=1)
+
 
 
