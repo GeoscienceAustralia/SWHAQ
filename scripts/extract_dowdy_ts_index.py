1966a7c844b06115dc566117f8347b68ca874ec6
Kieran Ricardo <kieranricardo@hotmail.com>
2021-11-23 09:58:42 +1100

    bug fix

--- scripts/extract_dowdy_ts_index.py
+++ scripts/extract_dowdy_ts_index.py
@@ -5,32 +5,6 @@ from calendar import monthrange
 from mpi4py import MPI
 
 
-def dask_index(arr, indexes):
-    # assumes doing the second axis
-    sz1 = arr.data.shape[1] * arr.data.shape[2] * arr.data.shape[3]
-    sz2 = indexes.shape[1] * indexes.shape[2]
-    idxs = indexes.data.flatten() * sz2 + sz1 * (np.arange(indexes.size) // sz2) + np.arange(indexes.size) % sz2
-    # sort indices for speed
-    y = idxs.argsort()
-    i = np.empty_like(y)
-    i[y] = np.arange(y.size)
-    return arr.data.flatten()[idxs[y]].compute()[i].reshape(indexes.shape)
-
-
-def dask_interpolate(u, v, z, height):
-    top = height * 9.80665
-    idxs = (abs(top - z) + 1_000_000_000 * (z > top)).argmin(axis=1).compute()
-    z1 = dask_index(z, idxs)
-    u1 = dask_index(u, idxs)
-    v1 = dask_index(v, idxs)
-    z2 = dask_index(z, idxs - 1)
-    u2 = dask_index(u, idxs - 1)
-    v2 = dask_index(v, idxs - 1)
-    u_interp = (u2 * (top - z1) + u1 * (z2 - top)) / (z2 - z1)
-    v_interp = (v2 * (top - z1) + v1 * (z2 - top)) / (z2 - z1)
-    return u_interp, v_interp
-
-
 comm = MPI.COMM_WORLD
 
 pl_prefix = "/g/data/rt52/era5/pressure-levels/reanalysis"
@@ -57,13 +31,13 @@ for year in rank_years:
         if not os.path.isfile(ufile):
             continue
 
-        u = xr.open_dataset(ufile, chunks='auto').u.sel(longitude=long_slice, latitude=lat_slice)
-        v = xr.open_dataset(vfile, chunks='auto').v.sel(longitude=long_slice, latitude=lat_slice)
+        u = xr.open_dataset(ufile, chunks='auto').u.sel(longitude=long_slice, latitude=lat_slice).compute()
+        v = xr.open_dataset(vfile, chunks='auto').v.sel(longitude=long_slice, latitude=lat_slice).compute()
         cape = xr.open_dataset(capefile, chunks='auto').cape.sel(longitude=long_slice, latitude=lat_slice).compute()
         u10 = xr.open_dataset(u10file, chunks='auto').u10.sel(longitude=long_slice, latitude=lat_slice).compute()
         v10 = xr.open_dataset(v10file, chunks='auto').v10.sel(longitude=long_slice, latitude=lat_slice).compute()
         totalx = xr.open_dataset(totalxfile, chunks='auto').totalx.sel(longitude=long_slice, latitude=lat_slice).compute()
-        z = xr.open_dataset(zfile, chunks='auto').z.sel(longitude=long_slice, latitude=lat_slice)
+        z = xr.open_dataset(zfile, chunks='auto').z.sel(longitude=long_slice, latitude=lat_slice).compute()
 
         uu = u.sel(level=500).compute() - u10
         vv = v.sel(level=500).compute() - v10
@@ -92,9 +66,6 @@ for year in rank_years:
         days = u.coords['time'].data.reshape((-1, 24))[:, 0]
 
         data_vars = {
-            'mason': (('time', 'latitude', 'longitude'), mason),
-            'allen': (('time', 'latitude', 'longitude'), allen),
-            'totalx': (('time', 'latitude', 'longitude'), totalx),
         }
 
         coords = {
