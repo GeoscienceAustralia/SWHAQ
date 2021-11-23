6198f00de4627ddcaea9c3563c009ac8c5093a60
Kieran Ricardo <kieranricardo@hotmail.com>
2021-11-23 15:53:04 +1100

    dowdy index

--- scripts/extract_dowdy_ts_index.py
+++ scripts/extract_dowdy_ts_index.py
@@ -25,6 +25,89 @@ def calc_rhmin13(height_profile, rh_profile):
     return rh_profile[mask].min()
 
 
+def calc_dewpoint(pressure, temperature, relative_humidity):
+    mixing_ratio = metpy.calc.mixing_ratio_from_relative_humidity(
+        pressure, temperature, relative_humidity
+    )
+    vapor_pressure = metpy.calc.vapor_pressure(pressure, mixing_ratio)
+    dewpoint = metpy.calc.dewpoint(vapor_pressure)
+    return dewpoint
+
+
+def calc_effective_layer(pressure, temperature, dewpoint):
+
+    bottom_idx = None
+    top_idx = None
+
+    # start at ground level and increment pressure levels
+    for parcel_idx in range(len(pressure) - 1, -1, -1):
+        p, t, td, mu_profile = metpy.calc.parcel_profile_with_lcl(
+            pressure[parcel_idx:], temperature[parcel_idx:], dewpoint[parcel_idx:]
+        )
+
+        cape, cin = metpy.calc.cape_cin(p, t, td, mu_profile)
+
+        if (cape > 100) and (cin > -250):
+            # if this is the first pressure level (from ground) to satisfy the constraints
+            # this is the bottom of the inflow layer
+            if bottom_idx is None:
+                bottom_idx = parcel_idx
+        else:
+            # if this is the first pressure level (from base of inflow level) that violates
+            # the constraints this is the end of the inflow layer
+            if bottom_idx is not None:
+                top_idx = bottom_idx
+                break
+
+    return bottom_idx, top_idx
+
+
+def calc_windpeed_mean(u, v, pressure):
+    mask = (600 <= pressure) & (pressure <= 800)
+    umean = metpy.calc.mean_pressure_weighted(
+        pressure[mask], u[mask]
+    )
+    vmean = metpy.calc.mean_pressure_weighted(
+        pressure[mask], v[mask]
+    )
+    windspeed_mean = metpy.calc.wind_speed(umean, vmean)
+    return windspeed_mean
+
+
+def calc_dowdy(u, v, pressure, temperature, height, relative_humidity):
+    windspeed_mean = calc_windpeed_mean(u, v, pressure)
+    lr13 = calc_lr13(height, temperature)
+    rhmin13 = calc_rhmin13(height, relative_humidity)
+
+    # find effective layer
+    dewpoint = calc_dewpoint(pressure, temperature, relative_humidity)
+    bottom_idx, top_idx = calc_effective_layer(pressure, temperature, dewpoint)
+
+    srhe = metpy.calc.storm_relative_helicity(
+        height[top_idx:bottom_idx], u[top_idx:bottom_idx], v[top_idx:bottom_idx]
+    )
+
+    shear = metpy.calc.wind_speed(
+        *metpy.calc.bulk_shear(
+            pressure[top_idx:bottom_idx], u[top_idx:bottom_idx], v[top_idx:bottom_idx]
+        )
+    )
+
+    # how to average? bottom of
+    efflcl = metpy.calc.lcl(
+        pressure[bottom_idx], temperature[bottom_idx], dewpoint[bottom_idx]
+    )
+
+    qmelt = None
+
+    dowdy = 6.1e-02 * shear + 1.5e-1 * windspeed_mean + 9.4e-1 * lr13 + 3.9e-2 * rhmin13
+    dowdy += 1.7e-02 * srhe + 3.8e-1 * qmelt + 4.7e-4 * efflcl - 1.3e1
+    dowdy = 1 / (1 + dowdy.exp())
+
+    return dowdy
+
+
+
 comm = MPI.COMM_WORLD
 
 pl_prefix = "/g/data/rt52/era5/pressure-levels/reanalysis"
@@ -89,38 +172,41 @@ for year in rank_years:
                     rhmin13.data[i, :, j, k] = calc_rhmin13(height_profile, rh_profile)
 
                     # find effective layer
+                    dewpoint = calc_dewpoint(pressure_profile, temp_profile, rh_profile)
+                    bottom_idx, top_idx = calc_effective_layer(pressure_profile, temp_profile, dewpoint)
 
-                    srhe[i, j, k] = metpy.calc.storm_relative_helicity(height_profile, u_profile, v_profile)
-                    shear[i, j, k] = metpy.calc.wind_speed(
-                        *metpy.calc.bulk_shear(pressure_profile, u_profile, v_profile)
+
+                    srhe.data[i, j, k] = metpy.calc.storm_relative_helicity(
+                        height_profile[top_idx:bottom_idx], u_profile[top_idx:bottom_idx],
+                        v_profile[top_idx:bottom_idx]
                     )
 
-                    # find effective
-                    # efflcl.data[i, j, k] = metpy.calc.lcl(pressure, temperature, dewpoint)
+                    shear.data[i, j, k] = metpy.calc.wind_speed(
+                        *metpy.calc.bulk_shear(
+                            pressure_profile[top_idx:bottom_idx], u_profile[top_idx:bottom_idx],
+                            v_profile[top_idx:bottom_idx]
+                        )
+                    )
 
+                    # how to average? bottom of
+                    efflcl.data[i, j, k] = metpy.calc.lcl(
+                        pressure_profile[bottom_idx], temp_profile[bottom_idx], dewpoint[bottom_idx]
+                    )
 
+        dowdy = 6.1e-02 * shear + 1.5e-1 * umean + 9.4e-1 * lr13 + 3.9e-2 * rhmin13
+        dowdy += 1.7e-02 * srhe + 3.8e-1 * qmelt + 4.7e-4 * efflcl -1.3e1
+        dowdy = 1 / (1 + dowdy.exp())
 
-
-
-        lr13 = None # temperature lapse rate from 1-3km
-        rhmin13 = None # rel hum min 1-3km
-        srhe = None # effective-layer storm relative helicity
-        qmelt = None # water vapor mixing ratio at the height of the melting level
-        efflcl = None # effective-layer parcel lifting condensation level
-
-        # dowdy = 6.1e-02 * shear + 1.5e-1 * umean + 9.4e-1 * lr13 + 3.9e-2 * rhmin13
-        # dowdy += 1.7e-02 * srhe + 3.8e-1 * qmelt + 4.7e-4 * efflcl -1.3e1
-
-        days = u.coords['time'].data.reshape((-1, 24))[:, 0]
 
         data_vars = {
+            "dowdy": dowdy
         }
 
         coords = {
-            'time': days,
+            'time': u.coords['time'].data.reshape((-1, 24))[:, 0],
             'latitude': u.coords['latitude'].data,
             'longitude': u.coords['longitude'].data,
         }
 
         ds = xr.Dataset(data_vars, coords)
-        ds.to_netcdf(outpath + "ts_indices_{year}{month:02d}01-{year}{month:02d}{days}.nc")
+        ds.to_netcdf(outpath + f"ts_indices_{year}{month:02d}01-{year}{month:02d}{days}.nc")
