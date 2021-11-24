ac5fff8cf05f7c548b6e95db0123e7209f9fc870
Kieran Ricardo <kieranricardo@hotmail.com>
2021-11-25 10:47:15 +1100

    fixes to dowdy index calculation

--- scripts/calculate_ts_indices.sh
+++ scripts/calculate_ts_indices.sh
@@ -8,6 +8,7 @@
 #PBS -joe
 #PBS -lstorage=scratch/w85+gdata/w85+gdata/rt52+gdata/dk92
 
+
 module purge
 module load pbs
 module load dot
@@ -15,6 +16,9 @@ module load dot
 module use /g/data/dk92/apps/Modules/modulefiles
 module load NCI-data-analysis/2021.09
 module load openmpi/4.1.0
+
+module use /g/data/v10/public/modules/modulefiles
+module load dea/20210527
 
 cd $HOME/SWHAQ/scripts
 mpiexec -n 44 python3 extract_ts_indices.py
