24fc8e1ee337b0254986b5f215a8c5a5670eecc5
Kieran Ricardo <kieranricardo@hotmail.com>
2022-01-28 10:28:42 +1100

    compare ts indices and lightnint

--- scripts/ts_indices/compare_indices_lightning.py
+++ scripts/ts_indices/compare_indices_lightning.py
@@ -5,7 +5,7 @@ import numpy as np
 import xarray as xr
 from matplotlib import pyplot as plt
 
-DATA_DIR = "/g/data/w85/QFES_SWHA/hazard/tsindices"
+DATA_DIR = "/g/data/w85/QFES_SWHA/hazard/input"
 OUT_PATH = os.path.expanduser("/g/data/w85/QFES_SWHA/hazard/output/plots")
 
 # load in and plot indices
