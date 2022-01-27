4a142330e3b3db9cbad8d8c8ecfc37f640bb76f6
Kieran Ricardo <kieranricardo@hotmail.com>
2022-01-27 17:09:55 +1100

    aggregate tc indices

--- scripts/ts_indices/aggregate_ts_indices.py
+++ scripts/ts_indices/aggregate_ts_indices.py
@@ -1,16 +1,33 @@
 import os
 import xarray as xr
 
-out_arr = None
+out_ds = None
 path = "/g/data/w85/QFES_SWHA/hazard/input/tsindices/"
 
-for fn in os.listdir(path)[:3]:
+for fn in os.listdir(path)[:4]:
+    print("processing", fn)
     if '2021' in fn:
         # skip 2021 because its incomplete
         continue
 
-    if out_arr is None:
-        out_arr = xr.load_dataset(os.path.join(path, fn))
+    ds = xr.load_dataset(os.path.join(path, fn))
+    mason = (ds.mason > 33867).sum(dim='time')
+    allen = (ds.allen > 33867).sum(dim='time')
+    totalx = (ds.totalx > 48.1).sum(dim='time')
+
+    if out_ds is None:
+        out_ds = xr.Dataset(coords=ds.sum(dim='time').coords, attrs=ds.attrs)
+        out_ds['mason'] = mason
+        out_ds['allen'] = allen
+        out_ds['totalx'] = totalx
+    else:
+        out_ds['mason'] += mason
+        out_ds['allen'] += allen
+        out_ds['totalx'] += totalx
 
 
-print(out_arr)
+
+
+
+
+print(out_ds)
