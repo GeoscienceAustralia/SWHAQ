36a486f600d026b49c5a45603c97090c98e13f15
Kieran Ricardo <kieranricardo@hotmail.com>
2022-06-24 16:20:04 +1000

    wm script

--- scripts/apply_wind_multipliers.py
+++ scripts/apply_wind_multipliers.py
@@ -7,11 +7,12 @@ out_dir = "/g/data/w85/QFES_SWHA/hazard/output/wm_combined_aep"
 
 fn = "windspeed_200_yr.nc"
 
-wm = xr.open_rasterio("/g/data/w85/QFES_SWHA/multipliers/output/QLD/wind-multipliers-maxima.vrt", chunks='auto')
-ingust = xr.open_dataset(os.path.join(in_dir, fn), chunks="auto").interp(lon=wm.x.data, lat=wm.y.data)
-
-out = ingust.windspeed.data * wm.sel(band=1).data
-da = xr.DataArray(out, dims=ingust.dims, coords=ingust.coords)
-ds = xr.Dataset(dict(gust=da))
-c = ds.to_netcdf(os.path.join(out_dir, fn), compute=False)
-c.compute(num_workers=15)
+wm = xr.open_rasterio("/g/data/w85/QFES_SWHA/multipliers/output/QLD/wind-multipliers-maxima.vrt")
+print(wm.data.shape)
+# ingust = xr.open_dataset(os.path.join(in_dir, fn), chunks="auto").interp(lon=wm.x.data, lat=wm.y.data)
+#
+# out = ingust.windspeed.data * wm.sel(band=1).data
+# da = xr.DataArray(out, dims=ingust.dims, coords=ingust.coords)
+# ds = xr.Dataset(dict(gust=da))
+# c = ds.to_netcdf(os.path.join(out_dir, fn), compute=False)
+# c.compute(num_workers=15)
