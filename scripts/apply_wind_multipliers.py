aa341f3e033d143bab1498df1836ccc79386f981
Kieran Ricardo <kieranricardo@hotmail.com>
2022-06-24 15:51:36 +1000

    wm script

--- scripts/apply_wind_multipliers.py
+++ scripts/apply_wind_multipliers.py
@@ -1,16 +1,17 @@
 import xarray as xr
 import os
 
-wm = xr.open_rasterio("/g/data/w85/QFES_SWHA/multipliers/output/QLD/wind-multipliers-maxima.vrt")
 
 in_dir = "/g/data/w85/QFES_SWHA/hazard/output/combined_aep"
 out_dir = "/g/data/w85/QFES_SWHA/hazard/output/wm_combined_aep"
 
 fn = "windspeed_200_yr.nc"
-ingust = xr.open_dataset(os.path.join(in_dir, fn)).interp(lon=wm.x.data, lat=wm.y.data)
+
+wm = xr.open_rasterio("/g/data/w85/QFES_SWHA/multipliers/output/QLD/wind-multipliers-maxima.vrt", chunks='auto')
+ingust = xr.open_dataset(os.path.join(in_dir, fn), chunks="auto").interp(lon=wm.x.data, lat=wm.y.data)
 
 out = ingust.windspeed.data * wm.sel(band=1).data
 da = xr.DataArray(out, dims=ingust.dims, coords=ingust.coords)
 ds = xr.Dataset(dict(gust=da))
-c = ds.to_netcdf(os.path.join(out_dir, fn), compute=True)
-# c.compute(num_workers=4)
+c = ds.to_netcdf(os.path.join(out_dir, fn), compute=False)
+c.compute(num_workers=15)
--- scripts/run_apply_wind_multipliers.sh
+++ scripts/run_apply_wind_multipliers.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #PBS -P w85
-#PBS -qnormal
+#PBS -q normal
 #PBS -N wm_max_aep
 #PBS -m abe
 #PBS -M kieran.ricardo@ga.gov.au
 #PBS -l walltime=0:14:00
-#PBS -l mem=100GB,ncpus=8,jobfs=4000MB
+#PBS -l mem=100GB,ncpus=16,jobfs=4000MB
 #PBS -W umask=0002
 #PBS -joe
 #PBS -l storage=gdata/v10+gdata/w85
