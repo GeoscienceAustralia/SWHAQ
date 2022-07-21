f9e0f456c1ffdf357c358db662c8780bc1f305d8
wcarthur <wcarthur@gmail.com>
2022-07-21 14:03:32 +1000

    More tweaks to the plots

--- scripts/visualise_aep_windspeed.py
+++ scripts/visualise_aep_windspeed.py
@@ -1,6 +1,8 @@
 import os
 from matplotlib import pyplot as plt
 from matplotlib import patheffects
+from matplotlib.ticker import ScalarFormatter, LogLocator, NullFormatter
+
 import xarray as xr
 import pandas as pd
 import numpy as np
@@ -8,6 +10,8 @@ from ari_interpolate import gdp_recurrence_intervals
 import hazard
 import seaborn as sns
 sns.set_context("talk")
+majloc = LogLocator()
+minloc = LogLocator(base=10.0, subs=np.arange(0.1, 1, 0.1), numticks=12)
 pe = patheffects.withStroke(foreground="white", linewidth=5)
 
 datadir = "/g/data/w85/QFES_SWHA/hazard/output/wm_combined_aep"
@@ -24,7 +28,6 @@ syn_aep = np.interp(windspeeds, df.Synoptic.values, df.aep.values)
 syn_aep[windspeeds > df.Synoptic.max()] = 0.0
 ts_aep = np.interp(windspeeds, df.Thunderstorm.values, df.aep.values)
 ts_aep[windspeeds > df.Thunderstorm.max()] = 0.0
-
 i = 14
 tc_df = pd.read_csv(os.path.join(IN_DIR, "tc_ari_params", "parameters.csv"))
 tc_df.columns = [c.strip() for c in tc_df.columns]
@@ -34,17 +37,22 @@ tc_aep[windspeeds <= hazard.GPD.gpdReturnLevel(1, mu, shape, scale, rate)] = 0.0
 
 comb_aep_ = 1.0 - (1.0 - syn_aep) * (1.0 - ts_aep) * (1.0 - tc_aep)
 
-plt.figure(figsize=(10, 10))
+fig, ax = plt.subplots(1, 1, figsize=(12.8, 9.6))
 plt.title(f"{tc_df.iloc[i].locName} AEP")
 plt.semilogy(windspeeds, syn_aep, label="Synoptic", path_effects=[pe])
 plt.semilogy(windspeeds, ts_aep, label="Thunderstorm", path_effects=[pe])
 plt.semilogy(windspeeds, tc_aep, label="Tropical Cyclone", path_effects=[pe])
 plt.semilogy(windspeeds, comb_aep_, label="Combined", path_effects=[pe])
-plt.xlabel("Windspeed (m/s)")
-plt.ylabel("AEP")
+plt.xlabel("Wind speed [m/s]")
+plt.ylabel("Annual exceedance probability [%]")
 plt.ylim((10e-5, 1))
-plt.grid()
+ax.yaxis.set_major_locator(majloc)
+ax.yaxis.set_minor_locator(minloc)
+ax.yaxis.set_minor_formatter(NullFormatter())
+plt.grid(which='major', linestyle='-')
+plt.grid(which='minor', linestyle='--', linewidth=1)
 plt.legend()
+fig.tight_layout()
 plt.savefig(os.path.join(datadir, f"windspeed_aep_{tc_df.iloc[i].locName}.png"))
 
 outdf = pd.DataFrame(np.array([windspeeds, syn_aep, ts_aep, tc_aep, comb_aep_]).T,
