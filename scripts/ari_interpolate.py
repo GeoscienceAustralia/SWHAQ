90619994c4b49927383cc68597b7ad47c1aea607
wcarthur <wcarthur@gmail.com>
2022-07-27 08:50:44 +1000

    Use plotting position AEP for TC

--- scripts/ari_interpolate.py
+++ scripts/ari_interpolate.py
@@ -1,17 +1,17 @@
 import os
-
-IN_DIR = os.path.expanduser('/g/data/w85/QFES_SWHA/hazard/input')
-OUT_DIR = os.path.expanduser('/g/data/w85/QFES_SWHA/hazard/output')
-
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import sys
 import geopandas as gpd
 import xarray as xr
+from scipy.interpolate import interp1d
+from pykrige import OrdinaryKriging
+
 import hazard
 
-
+IN_DIR = os.path.expanduser('/g/data/w85/QFES_SWHA/hazard/input')
+OUT_DIR = os.path.expanduser('/g/data/w85/QFES_SWHA/hazard/output')
 
 def gdp_recurrence_intervals(return_levels, mu, shape, scale, rate, npyr=365.25):
     """
@@ -31,7 +31,6 @@ def gdp_recurrence_intervals(return_levels, mu, shape, scale, rate, npyr=365.25)
 
     """
 
-
     ri = np.power((return_levels - mu) * (shape / scale) + 1, 1 / shape) / (npyr * rate)
     if not np.isscalar(ri):
         ri[np.isnan(ri)] = np.inf
@@ -39,23 +38,47 @@ def gdp_recurrence_intervals(return_levels, mu, shape, scale, rate, npyr=365.25)
         ri = np.inf
     return ri
 
-if __name__ == "__main__":
-    from pykrige import OrdinaryKriging
+def interpgrid(data, windspeeds, aeps):
+    """
+    Interpolate wind speeds to defined AEPs. We treat the exceedance
+    probabilities as the 
 
-    # load in a process synoptic and ts ARI data
+    :param data: :class:`np.ndarray` of the combined AEP at defined wind speeds
+        across the domain.
+    :param windspeeds: :class:`np.array` of the wind speeds. Must be the same
+        length as data.shape[-1]
+    :param aeps: :class:`np.array` containing the target exceedance
+        probabilities
+    
+    :returns: :class:`np.ndarray` of the wind speeds at the defined AEPs over
+        the grid
+
+    """
+    nx, ny = data.shape[:-1]
+    out = np.zeros((nx, ny, len(aeps)))
+
+    for x in range(nx):
+        for y in range(ny):
+            f = interp1d(data[x, y, :], windspeeds, bounds_error=False, fill_value="extrapolate")
+            out[x, y, :] = f(aeps)
+    return out
+
+if __name__ == "__main__":
+
+    # load in a process synoptic and TS ARI data - provided by UQ (Matt Mason)
     fp = os.path.join(IN_DIR, "AllStationsSuperStation_20220622.xlsx")
     df = pd.read_excel(fp, skiprows=1).iloc[9:]
 
     df["aep"] = 1 - np.exp(-1 / df["ARI [yrs]"])
-    windspeeds = np.linspace(20, 100, 100)
+    windspeeds = np.linspace(20, 100, 101)
     syn_aep = np.interp(windspeeds, df.Synoptic.values, df.aep.values)
     syn_aep[windspeeds > df.Synoptic.max()] = 0.0
     ts_aep = np.interp(windspeeds, df.Thunderstorm.values, df.aep.values)
     ts_aep[windspeeds > df.Thunderstorm.max()] = 0.0
 
-    # load TC ARI curves for stations
+    # load TC ARI curves for stations:
     stnlist = gpd.read_file(os.path.join(IN_DIR, "stations", "SEQ_station_list.shp"))
-    tc_df = pd.read_csv(os.path.join(IN_DIR, "tc_ari_params", "parameters.csv"))
+    tc_df = pd.read_csv(os.path.join(IN_DIR, "tc_ari_params_", "parameters.csv"))
 
     tc_df.columns = [c.strip() for c in tc_df.columns]
     tc_df['longitude'] = stnlist.loc[tc_df.locId.values - 1].Longitude.values
@@ -73,18 +96,23 @@ if __name__ == "__main__":
         shape, scale, rate, mu = tc_df.iloc[i][['it_shape', 'it_scale', 'it_rate', 'it_thresh']].values
         params.append([shape, scale, rate, mu])
     params = np.array(params)
-
     # SEQ grid
-    lats = np.linspace(-10, -30, 1000)
-    longs = np.linspace(137, 154, 1000)
+    lats = np.linspace(-24, -30, 300)
+    longs = np.linspace(151, 155, 200)
 
     # station lat and longs
     stn_lats = tc_df.latitude.values
     stn_longs = tc_df.longitude.values
 
+    tc_aep_pp = np.zeros((len(tc_df), len(windspeeds)))
+    for i, (idx, row) in enumerate(tc_df.iterrows()):
+        tc_aep_df =  pd.read_csv(os.path.join(IN_DIR, "tc_ari_params_", f"{row.locId}.csv"))
+        finterp = interp1d(tc_aep_df.wspd.values, tc_aep_df.AEP.values, 
+                   bounds_error=False, fill_value=tc_aep_df.AEP.min())
+        tc_aep_pp[i, :] = finterp(windspeeds)
+
     param_names = ['shape', 'scale', 'rate', 'mu']
     grid_params = dict()
-
     for j in range(4):
         OK = OrdinaryKriging(
             stn_longs,
@@ -98,6 +126,21 @@ if __name__ == "__main__":
         z, _ = OK.execute("grid", longs, lats)
         grid_params[param_names[j]] = z
 
+    tc_aep_grid = np.zeros((len(lats),
+                            len(longs),
+                            len(windspeeds)))
+    for j in range(len(windspeeds)):
+        OK = OrdinaryKriging(
+            stn_longs,
+            stn_lats,
+            tc_aep_pp[:, j],
+            coordinates_type='geographic',
+            pseudo_inv=True,
+            verbose=False,
+            enable_plotting=False,
+        )
+        z, _ = OK.execute("grid", longs, lats)
+        tc_aep_grid[:, :, j] = z
 
     # combine the AEP for TC, synoptic and TS
     shape, scale = grid_params["shape"][:, :, None], grid_params["scale"][:, :, None]
@@ -106,7 +149,7 @@ if __name__ == "__main__":
     print("max windspeed:", hazard.GPD.gpdReturnLevel(10_000, mu, shape, scale, rate).max())
 
     tc_aep = 1.0 - np.exp(-1 / gdp_recurrence_intervals(windspeeds[None, None, :], mu, shape, scale, rate).data)
-    comb_aep = 1.0 - (1.0 - syn_aep[None, None, :]) * (1.0 - ts_aep[None, None, :]) * (1.0 - tc_aep)
+    comb_aep = 1.0 - (1.0 - syn_aep[None, None, :]) * (1.0 - ts_aep[None, None, :]) * (1.0 - tc_aep_grid)
 
     # convert windspeed coords + AEP values to AEP coords and windspeed values
     # funky code to quickly linearly interpolate (a for loop + numpy interpolate was much much slower)
@@ -117,17 +160,25 @@ if __name__ == "__main__":
     )
 
     aeps = 1 - np.exp(-1 / ris)
+    outda = interpgrid(comb_aep, windspeeds, aeps)
+    for idx, (ri, aep) in enumerate(zip(ris, aeps)):
+        da = xr.DataArray(
+            outda[:,:,idx],
+            coords=dict(lon=longs, lat=lats),
+            dims=["lat", "lon"]
+        )
+        ds = xr.Dataset(data_vars={'windspeed': da})
+        ds.to_netcdf(os.path.join(OUT_DIR, "combined_aep", f"windspeed_{ri}_yr.nc"))
 
+"""
     comb_aep_flat = comb_aep.reshape((-1, len(windspeeds)))
     grid_idxs = np.arange(comb_aep_flat.shape[0])
     aep_windspeed_grid = []
-
     for ri, aep in zip(ris, aeps):
 
         mask = comb_aep >= aep
         idxs = np.where(np.diff(mask))[2]
-        # print(ri, aep, len(idxs))
-
+        print(ri, aep, len(idxs), idxs.min(), idxs.max())
         w1 = comb_aep_flat[grid_idxs, idxs] - aep
         w2 = aep - comb_aep_flat[grid_idxs, idxs + 1]
         w = w1 + w2
@@ -135,12 +186,15 @@ if __name__ == "__main__":
         w2 /= w
 
         aep_ws = w1 * windspeeds[idxs] + w2 * windspeeds[idxs + 1]
-
+        
+        
         da = xr.DataArray(
             aep_ws.reshape(comb_aep.shape[:-1]),
             coords=dict(lon=longs, lat=lats),
             dims=["lat", "lon"]
         )
+
         ds = xr.Dataset(data_vars={'windspeed': da})
 
         ds.to_netcdf(os.path.join(OUT_DIR, "combined_aep", f"windspeed_{ri}_yr.nc"))
+"""
