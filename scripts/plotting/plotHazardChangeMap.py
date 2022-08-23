7176d1cb59f762d97213322afcb5c65d0ea488e7
wcarthur <wcarthur@gmail.com>
2022-08-23 11:57:33 +1000

    Move plotting scripts to a subfolder

--- scripts/plotting/plotHazardChangeMap.py
+++ scripts/plotting/plotHazardChangeMap.py
@@ -11,13 +11,56 @@ import cartopy.feature as cfeature
 
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
+from scipy import signal
+
+def gaussKern(size):
+    """
+    Calculate a normalised Gaussian kernel to apply as a smoothing
+    function.
+
+    :param int size: the size of the kernel to use (how many points will be
+                used in the smoothing operation).
+    :returns: :class:`numpy.ndarray` normalised 2D kernel array for use in
+               convolutions
+    """
+    size = int(size)
+    x, y = np.mgrid[-size:size + 1, -size:size + 1]
+    g = np.exp(-(x**2/float(size) + y**2/float(size)))
+    return g / g.sum()
+
+def smooth(im, n=15):
+    """
+    Smooth a 2D array `im` by convolving with a Gaussian kernel of size `n`.
+
+    :param im: Array of values to be smoothed
+    :type  im: :class:`numpy.ndarray`
+    :param int n: Number of points to include in the smoothing.
+
+    :returns: smoothed array (same dimensions as the input array)
+
+    """
+    g = gaussKern(n)
+    improc = signal.convolve2d(im, g, mode='same', boundary='symm')
+    return improc
+
+def subset(ds, extent):
+
+    data = ds.sel(
+        lat=slice(extent[3], extent[2]),
+        lon=slice(extent[0], extent[1])
+    )
+    return data
+
 
 datapath = '/scratch/w85/swhaq/hazard/output/QLD'
 datapath = r"X:\georisk\HaRIA_B_Wind\projects\qfes_swha\data\derived\hazard\projections"
+datapath = '/g/data/w85/QFES_SWHA/hazard/output/'
 groups = ['GROUP1', 'GROUP2']
 rcps = ['RCP45', 'RCP85']
 periods = ['2021-2040', '2041-2060', '2061-2080', '2081-2100']
 
+extent = (150, 155, -30, -24)
+bbox = dict(boxstyle="round", fc="white", alpha=0.5)
 g = 'GROUP1'
 rlabels = {'RCP45': "RCP 4.5", "RCP85": "RCP 8.5"}
 p = '2081-2100'
@@ -39,14 +82,18 @@ for g in groups:
             fname = os.path.join(datapath, scenario, 'hazard', 'hazard_change.nc')
             print(f"Processing {fname}")
             ds = xr.open_dataset(fname)
+            lats = ds.lat.sel(lat=slice(extent[3], extent[2])).values
+            lons = ds.lon.sel(lon=slice(extent[0], extent[1])).values
+            lat, lon = np.meshgrid(lats, lons)
+            dx = np.mean(np.diff(ds.lon.values))
             for ari in aris:
                 fig, ax = plt.subplots(1, 1, subplot_kw={'projection':prj})
                 title = f"1:{ari} AEP difference - {p}"
-                ds.wspd.sel({'ari':ari}).plot.contourf(
-                    levels=levels, extend='both',
-                    add_labels=True,
-                    cbar_kwargs={'label':"Relative change in AEP wind speed [%]"},
-                    ax=ax)
+                data = subset(ds.wspd.sel({'ari': ari}), extent)
+                sdata = smooth(data, int(1/dx))
+                cs = ax.contourf(lon, lat, sdata.T, levels=levels, extend='both', cmap='RdBu_r')
+                plt.colorbar(cs, extend='both', label="Change in AEP wind speed [m/s]", ax=ax)
+                ax.set_extent(extent)
                 ax.coastlines(resolution='10m')
                 ax.add_feature(borders, edgecolor='k', linewidth=0.5)
                 gl = ax.gridlines(draw_labels=True, linestyle='--')
@@ -77,9 +124,13 @@ for p in periods:
             fname = os.path.join(datapath, scenario, 'hazard', 'hazard_change.nc')
             ds = xr.open_dataset(fname)
             title = f"{g} {rlabels[r]}"
-            im = ds.wspd.sel({'ari':ari}).plot.contourf(levels=levels, extend='both',
-                                                   add_labels=True, add_colorbar=False,
-                                                   ax=ax[i])
+            data = subset(ds.wspd.sel({'ari': ari}), extent)
+            lats = ds.lat.sel(lat=slice(extent[3], extent[2])).values
+            lons = ds.lon.sel(lon=slice(extent[0], extent[1])).values
+            lat, lon = np.meshgrid(lats, lons)
+            sdata = smooth(data, int(1/dx))
+            cs = ax[i].contourf(lon, lat, sdata.T, levels=levels, extend='both', cmap='RdBu_r')
+            ax[i].set_extent(extent)
             ax[i].coastlines(resolution='10m')
             ax[i].add_feature(borders, edgecolor='k', linewidth=0.5)
             gl = ax[i].gridlines(draw_labels=True, linestyle='--')
@@ -89,11 +140,11 @@ for p in periods:
             gl.yformatter = LATITUDE_FORMATTER
             gl.xlabel_style = {'size': 'x-small'}
             gl.ylabel_style = {'size': 'x-small'}
-            ax[i].set_title(title, fontsize='small')
-        fig.subplots_adjust(right=0.85, wspace=0.1, hspace=0.05, top=0.95)
+            ax[i].text(0.5, 0.95, title, ha='center', va='center', fontsize='small', transform=ax[i].transAxes, bbox=bbox)
+        fig.subplots_adjust(right=0.875, wspace=0.1, hspace=0.05, top=0.95)
         cbar_ax = fig.add_axes([0.9, 0.15, 0.025, 0.7])
         cbarlabel = "Change in AEP wind speed [m/s]"
-        fig.colorbar(im, cax=cbar_ax, label=cbarlabel)
+        fig.colorbar(cs, cax=cbar_ax, label=cbarlabel)
         #plt.tight_layout()
         fig.suptitle(suptitle)
         outputfile = os.path.join(datapath, f'hazard_change.{ari}.{p}.png')
