3ae5bb96d784f6957ad5200fbc2e5a93a7a05d86
Craig Arthur <wcarthur@gmail.com>
2022-10-26 15:53:46 +1100

    Add AS4055 classification for Region A

--- scripts/prepare_exposure.py
+++ scripts/prepare_exposure.py
@@ -2,17 +2,17 @@
 prepare_exposure.py
 
 This script reads in a NEXIS TCRM exposure file, and assigns a vulnerability
-function based on the supplied suite of curves from JCU CTS and VRMS. 
+function based on the supplied suite of curves from JCU CTS and VRMS.
 
 To do this, we first assign a nominal site classification based on AS 4055
 (i.e. N1-N6 and C1-C4) to each building. The values used for defining the site
 classification are taken from GA's site exposure product
 (http://pid.geoscience.gov.au/dataset/ga/145891). The values in that dataset
 were determined using definitions in AS/NZS 1170.2 (2011), which are (largely)
-the same as for AS 4055. 
+the same as for AS 4055.
 
 Once the AS4055 classification is complete, a combination of that class, the
-roof type and construction era are used to set the vulnerability function. 
+roof type and construction era are used to set the vulnerability function.
 
 The mapping of (AS4055 class, roof type, construction era) to vulnerability
 function is set out in a spreadsheet supplied by JCU CTS and VRMS (Martin
@@ -21,12 +21,12 @@ Wehner [GA], David Henderson [JCU CTS]).
 We only address region B buildings, and then only building stock commonly
 found in southeast Queensland. Arguably this script could be used to assign
 curves to other regional building stock (e.g. north Queensland), but that would
-require updating the `curvemap` dict with appropriate mappings. The building 
+require updating the `curvemap` dict with appropriate mappings. The building
 attributes may also be different from those used here.
 
-Finally, this is a hack. A more robust technique that is built into the 
+Finally, this is a hack. A more robust technique that is built into the
 NEXIS TCRM extraction process (Location Information Section) is being
-considered. 
+considered.
 
 Contact:
 Craig Arthur
@@ -39,7 +39,7 @@ import pandas as pd
 
 def buildingClass(df, classes, thresholds, AS1170='C'):
     """
-    Assign a site classification (AS4055) based on wind loading region 
+    Assign a site classification (AS4055) based on wind loading region
     (AS1170.2) and local site multiplier value ('M4')
 
     :param df: :class:`pd.DataFrame` containing exposure data, with minimum set
@@ -47,27 +47,34 @@ def buildingClass(df, classes, thresholds, AS1170='C'):
         'M4']
     :param list classes: Labels to apply to each category
     :param list thresholds: Values that define the categories
-    :param str AS1170: AS/NZS 1170.2 wind region. 
+    :param str AS1170: AS/NZS 1170.2 wind region.
 
     :NOTE: the label for the wind region classification in some versions of the
         NEXIS TCRM files omits an 'I' in 'CLASSIFICATION'. Check the attribute
         names!
 
     """
-    df.loc[df['WIND_REGION_CLASSIFCATION'] == AS1170, 'AS4055_CLASS'] = \
-        pd.cut(df[df['WIND_REGION_CLASSIFCATION']==AS1170]['M4'],
+    df.loc[df['WIND_REGION_CLASSIFICATION'] == AS1170, 'AS4055_CLASS'] = \
+        pd.cut(df[df['WIND_REGION_CLASSIFICATION']==AS1170]['M4'],
                bins=thresholds,
                right=True,
                labels=classes)
     return df
-    
+
 datapath = r"X:\georisk\HaRIA_B_Wind\projects\qfes_swha\data\derived\exposure\2021"
-filename = "SEQ_ResidentialExposure_NEXIS_2021_M4.csv"
+datapath = r"X:\georisk\HaRIA_B_Wind\projects\qfes_swha\data\derived\exposure\2022"
+filename = "SEQ_Residential_Wind_Exposure_2021_TCRM_2022_VulnCurves_AS4055_M4.csv"
+#filename = "SEQ_ResidentialExposure_NEXIS_2021_M4.csv"
 df = pd.read_csv(os.path.join(datapath, filename), low_memory=False)
 
 # Set any data < 0 to default value - assume N2 classification
 df.loc[df['M4'] < 0., 'M4'] = 0.8
 #df.drop('M42', axis=1, inplace=True)
+
+# Apply a basic AS4055 class, based on the M4 value
+thresholds = [0.0, 0.8413, 1.0018, 1.2668, 1.5997, 2.]
+classes = ['N1', 'N2', 'N3', 'N4', 'N5']
+df = buildingClass(df, classes, thresholds, 'A')
 
 # Assign AS4055 classes - overwrite existing data!!!
 thresholds = [0.0, 0.8109, 1.0063, 1.2209, 1.4334, 2.]
@@ -105,7 +112,7 @@ curvemap = {
     ('N4', 'Tiles', '1982 - 1996'): 'dw362',
     ('N5', 'Tiles', '1982 - 1996'): 'dw363',
     ('N6', 'Tiles', '1982 - 1996'): 'dw364',
-        
+
 }
 
 
@@ -114,7 +121,7 @@ df2['idx'] = df2.index
 df2['WIND_VULNERABILITY_FUNCTION_ID'] = df2['idx'].map(curvemap)
 df2.drop('idx', axis=1, inplace=True)
 
-# Buggered if I know why this can't be more compact, but anyway. 
+# Buggered if I know why this can't be more compact, but anyway.
 # This just reverts the MultiIndex back to columns in the dataframe
 df2 = df2.reset_index([1, 2]).reset_index()
 
@@ -124,11 +131,11 @@ df2.loc[~df2.YEAR_BUILT.isin(['1997 - present', '1982 - 1996']) & \
         df2.WALL_TYPE.isin(['Brick Veneer', 'Double Brick']), 'WIND_VULNERABILITY_FUNCTION_ID'] = 'dw352'
 
 df2.loc[~df2.YEAR_BUILT.isin(['1997 - present', '1982 - 1996']) &
-       (df2.ROOF_TYPE=='Tiles'), 
+       (df2.ROOF_TYPE=='Tiles'),
        'WIND_VULNERABILITY_FUNCTION_ID'] = 'dw351'
 
 df2.loc[~df2.YEAR_BUILT.isin(['1997 - present', '1982 - 1996']) &
-       df2.ROOF_TYPE.isin(['Metal Sheeting', 'Fibro / asbestos cement sheeting']) & 
+       df2.ROOF_TYPE.isin(['Metal Sheeting', 'Fibro / asbestos cement sheeting']) &
        df2.WALL_TYPE.isin(['Timber', 'Fibro / asbestos cement sheeting']),
        'WIND_VULNERABILITY_FUNCTION_ID'] = 'dw350'
 
@@ -137,5 +144,5 @@ df2.WIND_VULNERABILITY_FUNCTION_ID.fillna(df2.TMPFUNC, inplace=True)
 df2.drop('TMPFUNC', axis=1, inplace=True)
 
 
-outputfile ="SEQ_ResidentialExposure_NEXIS_2021_M4_updated_v2.csv"
+outputfile ="SEQ_Residential_Wind_Exposure_2021_TCRM_2022_VulnCurves_AS4055_M4_updated_v2.csv"
 df2.to_csv(os.path.join(datapath, outputfile))
