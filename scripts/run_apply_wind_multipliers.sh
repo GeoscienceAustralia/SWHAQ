ef44572c13eace4adaaa472357cff70bc9f642f4
wcarthur <wcarthur@gmail.com>
2022-12-22 11:13:29 +1100

    Move clipping into the job script

--- scripts/run_apply_wind_multipliers.sh
+++ scripts/run_apply_wind_multipliers.sh
@@ -25,5 +25,19 @@ module load conda/analysis3
 cd /g/data/w85/QFES_SWHA/scripts
 python3 ari_interpolate.py
 python3 apply_wind_multipliers.py
-sh clip_windspeed_files.sh
+
+module load nco
+module load cdo
+
+cd /g/data/w85/QFES_SWHA/hazard/output/wm_combined_aep_pp
+
+for FILE in windspeed_*_yr.nc; do
+    OUTFILE=${FILE//.nc/_clip.nc}
+    echo $OUTFILE
+    ncks -O -d longitude,151.,154. -d latitude,-30.,-24. $FILE tmp.nc
+    ncatted -a GeoTransform,spatial_ref,m,c,"151.000138888696 0.0002777777779999919 0.0 -24.000138888696 0.0 -0.0002777777779999991" tmp.nc
+    cdo -O -L -s -setrtomiss,-999999.,0. -setmissval,-9999. tmp.nc $OUTFILE
+    rm tmp.nc
+done
+
 #python3 visualise_aep_windspeed.py
